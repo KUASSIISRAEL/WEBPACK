0a0150ed3240d7b626965ef1399cd2005014ccf5
nga-office <nga.office19@gmail.com>
2022-10-29 13:29:09 +0000

    ADD: ADD TEMPLATE TO THE INDEX.HTML

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,43 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [],
+  content: [
+    './index.html',
+    './**/*.{html,js}',
+    './src/*.{html,js}',
+    './src/**/*.{html,js}',
+  ],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    'colors': {
+      'plightBlack': '#7e7e7e',
+      'blacklight': '#242424',
+      'blackcardlight': '#2f2f2f',
+      'transparent': 'transparent',
+      'current': 'currentColor',
+      'black': colors.black,
+      'white': colors.white,
+      'gray': colors.gray,
+      'emerald': colors.emerald,
+      'indigo': colors.indigo,
+      'yellow': colors.yellow,
+      'green': colors.emerald,
+      'purple': colors.violet,
+      'pink': colors.fuchsia,
+      'orange': colors.orange,
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
   },
   plugins: [],
 }
